8d3136c48b840b69868fb316fe61d3a9290aa99d
Sampath Kumar <manepallisampatkumar@gmail.com>
2026-08-22 17:01:36 +0530

    Add strategy selection and registry

---
+++ strategies/registry.py
@@ -0,0 +1,13 @@
+class StrategyRegistry:
+
+    def __init__(self):
+        self.strategies = {}
+
+    def register(self, name, strategy):
+        self.strategies[name] = strategy
+
+    def get(self, name):
+        return self.strategies.get(name)
+
+    def all(self):
+        return self.strategies.copy()
